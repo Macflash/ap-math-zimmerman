fa695a2f42bea877db43443ab19727af7fc21697
Macflash <the.alex.breyfogle@gmail.com>
2021-11-03 12:46:52 -0700

    use double insted

---
+++ src/double.ts
@@ -0,0 +1,50 @@
+export function CreateDoubledArray(order: number) {
+    let rows: number[][] = [];
+    // rows start at ORDER, and increase by 1 per row, reaching (2*ORDER) - 1 then decrease
+    let rowLength = order;
+    for (let i = 0; i < order; i++) {
+        rows[i] = [];
+        for (let j = 0; j < rowLength; j++) {
+            rows[i][j] = 0;
+        }
+        rowLength++;
+    }
+    rowLength--;
+    for (let i = order; i < order * 2 - 1; i++) {
+        rows[i] = [];
+        rowLength--;
+        for (let j = 0; j < rowLength; j++) {
+            rows[i][j] = 0;
+        }
+    }
+
+    return rows;
+}
+
+export class DoubledCoord {
+    constructor(public x: number, public y: number) { }
+
+    static FromRowAndColumn(row: number, col: number) {
+        return new DoubledCoord(row, col * 2 - row);
+    }
+
+    toString() { return `${this.x}, ${this.y}` };
+
+    toRowAndColumn() {
+        let row = this.x;
+        let col = (this.y + this.x) / 2;
+        return { row, col };
+    }
+
+    static add(a: DoubledCoord, b: DoubledCoord) {
+        return new DoubledCoord(a.x + b.x, a.y + b.y);
+    }
+
+    static subtract(a: DoubledCoord, b: DoubledCoord) {
+        return new DoubledCoord(a.x - b.x, a.y - b.y);
+    }
+
+    static multiply(a: DoubledCoord, k: number) {
+        return new DoubledCoord(a.x * k, a.y * k);
+    }
+}
